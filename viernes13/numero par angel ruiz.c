0ea6730d4a75ad691ec5a71a954f5d0003113dd8
angelruiz230 <angel.ruiz4650@alumnos.udg.mx>
2026-02-13 08:42:28 -0600

    Add files via upload

---
+++ viernes13/numero par angel ruiz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int z;
+    printf("ingresa un numero:");
+    scanf("%d",&z);
+    if(z%2==0)
+    {
+        printf("\ntu numero es par");
+    }
+    //////////
+    else if(z%2!=0)
+    {
+        printf("\ntu numero es impar");
+    }
+    return 0;
+}
